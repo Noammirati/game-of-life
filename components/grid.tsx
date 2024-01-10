d509c4f9f85785005f6b7fca73910d163a017997
noammirati <noa.ammirati@gmail.com>
2024-01-10 12:27:08 +0100

    buttons in place, grid updating (maybe not correctly)

--- components/grid.tsx
+++ components/grid.tsx
@@ -4,9 +4,44 @@ import { useState } from 'react';
 
 
 export default function Grid() {
-    const [grid, setGrid] = useState(new Grid_());
+    const [grid, setGrid]: [any, Function] = useState(new Grid_());
+    const [intervalID, setIntervalID]: [any, Function] = useState(null);
 
-    function renderLine(line: Array<Cell_>, index : number) {
+    function update() {
+        console.log("UPDATE");
+        setGrid(grid.update())
+    }
+
+    function play() {
+        console.log("PLAY");
+        if (!intervalID) {
+            let int = setInterval(update, 500);
+            setIntervalID(int)
+        } else {
+            clearInterval(intervalID);
+            setIntervalID(null);
+        }
+    }
+
+    function randomize() {
+        console.log("RANDOMIZE");
+        setGrid(grid.randomize());
+    }
+
+    function clear() {
+        console.log("CLEAR");
+        let tgrid = grid.clear();
+        console.log("HERE", tgrid)
+        setGrid(tgrid);
+        console.log("THERE", grid)
+    }
+
+    function fill() {
+        console.log("Fill");
+        setGrid(grid.fill());
+    }
+
+    function renderLine(line: Array<Cell_>, index: number) {
         return (
             <div key={index} className='grid___line'>
                 {line.map((cell) => <Cell key={cell.getKey()} data={cell} />)}
@@ -14,15 +49,17 @@ export default function Grid() {
         )
     }
 
-    function update() {
-        console.log("UPDATE IN COMPONENT");
-        setGrid(grid.update())
-    }
-
     return (
-        <div className="grid">
+        <>
+            <button onClick={(e) => clear()}>Clear</button>
+            <button onClick={(e) => fill()}>Fill</button>
+            <button onClick={(e) => randomize()}>Randomize</button>
+            <button onClick={(e) => play()}>Play</button>
             <button onClick={(e) => update()}>Update</button>
-            {grid.getGrid().map((line, index) => renderLine(line, index))}
-        </div>
+
+            <div className='grid'>
+                {grid.getGrid().map((line: Cell_[], index: number) => renderLine(line, index))}
+            </div>
+        </>
     )
 }
--- src/grid.ts
+++ src/grid.ts
@@ -2,6 +2,7 @@ const SIZE = 100;
 const KEY_FORMAT = 'x:y';
 const COORDS_TO_KEY = (x: number, y: number) => KEY_FORMAT.replace('x', `${x}`).replace('y', `${y}`);
 
+enum cellState { dead, alive };
 export class Cell_ {
     private x: number;
     private y: number;
@@ -9,6 +10,7 @@ export class Cell_ {
     private neighbors: Array<Cell_>;
 
     init(grid: Grid_) {
+        console.log("cell init");
         let myKey = this.getKey();
         for (let i = -1; i < 2; i++) {
             for (let ii = -1; ii < 2; ii++) {
@@ -21,31 +23,28 @@ export class Cell_ {
                 }
             }
         }
-        if (this.x < 3) {
-
-            console.log(myKey, this.neighbors)
-        }
     }
 
     update() {
+        console.log("cell update");
         let aliveNeighbors = this.neighbors.filter((cell) => cell.isAlive());
-        console.log(this.getKey(), this.alive, aliveNeighbors.length)
         if (this.alive) {
             if (aliveNeighbors.length < 2) {
                 this.alive = false;
-                console.log("dying")
             }
             if (aliveNeighbors.length > 3) {
                 this.alive = false;
-                console.log("dying")
             }
         } else {
             if (aliveNeighbors.length = 3) {
                 this.alive = true;
-                console.log("aliving")
             }
         }
-        console.log(this.getKey(), this.alive)
+    }
+
+    clear() {
+        console.log("cell clear");
+        this.alive = false;
     }
 
     isAlive() {
@@ -56,10 +55,18 @@ export class Cell_ {
         return COORDS_TO_KEY(this.x, this.y);
     }
 
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, state?: cellState) {
+        console.log("cell constructor", state);
         this.x = x;
         this.y = y;
+
+        // Random state by default
         this.alive = Math.random() < 0.5;
+        if (!(typeof(state) === undefined)) {
+            if (state === cellState.alive) this.alive = true;
+            else if (state === cellState.dead) this.alive = false;
+        }
+
         this.neighbors = new Array<Cell_>;
     }
 }
@@ -69,38 +76,73 @@ export class Grid_ {
     private cells: Map<string, Cell_>;
 
     private initCells() {
+        console.log("grid initCells");
         const that = this;
         this.cells.forEach((cell) => cell.init(that))
     }
 
     getGrid() {
+        //console.log("grid getGrid");
         return this.gridCells;
     }
 
     getCell(key: string) {
+        //console.log("grid getCell", key);
         return this.cells.get(key);
     }
 
-    update() {
-        this.cells.forEach((cell) => cell.update());
-        console.log("UPDATING");
-        return this;
-    }
-
-    constructor() {
+    setCells(state?: cellState) {
+        console.log("grid setCells", state);
+        this.cells.clear();
         this.gridCells = new Array<Array<Cell_>>;
-        this.cells = new Map<string, Cell_>;
 
         for (let y = 0; y < (SIZE / 5) * 2; y++) {
             let row: Cell_[] = new Array<Cell_>();
             for (let x = 0; x <= SIZE; x++) {
-                let cell = new Cell_(x, y);
+                let cell = new Cell_(x, y, state);
                 this.cells.set(cell.getKey(), cell)
                 row.push(cell);
             }
             this.gridCells.push(row);
         }
 
-        this.initCells();
+    }
+
+    update() {
+        console.log("grid update");
+        this.cells.forEach((cell) => cell.update());
+        return new Grid_(this);
+    }
+
+    randomize() {
+        console.log("grid randomize");
+        this.setCells();
+        return new Grid_(this);
+    }
+
+    clear() {
+        console.log("grid clear");
+        this.setCells(cellState.dead);
+        console.log("CLEARED", this.cells)
+        return new Grid_(this);
+    }
+
+    fill() {
+        console.log("grid fill");
+        this.setCells(cellState.alive);
+        return new Grid_(this);
+    }
+
+    constructor(grid?: Grid_) {
+        console.log("grid constructor", grid);
+        if (grid) {
+            this.cells = grid.cells;
+            this.gridCells = grid.gridCells;
+        } else {
+            this.gridCells = new Array<Array<Cell_>>;
+            this.cells = new Map<string, Cell_>;
+            this.setCells();
+            this.initCells();
+        }
     }
 }
